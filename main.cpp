0a12b22b6b71070b0bd57c470ef2cbfc0941af01
ckblt <964463815@qq.com>
2021-05-29 13:57:43 +0800

    better typewriter

--- main.cpp
+++ main.cpp
@@ -9,46 +9,55 @@ string buffer = "";
 
 void openFile(char path[])
 {
-    // Read file
-    ifstream f(path);
-    
-    if (!f.is_open())
-    {
-        cout << "ERROR" << endl; // Output error
-        system("pause");
-        exit(0);
-    }
-    
-    char b[1024];
-    while (!f.eof())
-    {
-        f.getline(b, 1024);
-        read += b;
-        read += '\n';
-    }
+  // Read file
+  ifstream f(path);
 
-    // 4 space -> tab
-    while (read.find("    ") != read.npos)
-    {
-        read.replace(read.find("    "), 4, "\t");
-    }
+  if (!f.is_open())
+  {
+    cout << "ERROR" << endl; // Output error
+    system("pause");
+    exit(0);
+  }
+
+  char b[1024];
+  while (!f.eof())
+  {
+    f.getline(b, 1024);
+    read += b;
+    read += '\n';
+  }
+
+  // 4 space -> tab
+  while (read.find("    ") != read.npos)
+  {
+    read.replace(read.find("    "), 4, "\t");
+  }
 }
 
 void typewriter()
 {
-    for (int i = 0; i < read.length(); i++)
-    {
-        _getch(); // Wait until the key is pressed
-        system("cls"); // Clear screen
-        buffer += read[i];
-        cout << buffer;
-    }
+  for (int i = 0; i < read.length(); i++)
+  {
+    _getch();      // Wait until the key is pressed
+    system("cls"); // Clear screen
+    buffer += read[i];
+    cout << buffer;
+  }
+}
+
+void typewriterPlus()
+{
+  for (int i = 0; i < read.length(); i++)
+  {
+    _getch(); // Wait until the key is pressed
+    cout << read[i];
+  }
 }
 
 int main(int argc, char const *argv[])
 {
-    openFile("text.txt"); // Import file
-    typewriter();
-    system("pause"); // Pause, can be changed to ( while (true); )
-    return 0;
+  openFile("text.txt"); // Import file
+  typewriterPlus();
+  system("pause"); // Pause, can be changed to ( while (true); )
+  return 0;
 }
